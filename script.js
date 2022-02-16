ad87a195793270b83598481864e36e33a9b9c970
Jimmy <dabrowski.mac.1@gmail.com>
2022-02-16 15:29:57 +0100

    drawing gameboard works, clicking on divs works and and pushing to array works

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const game = (() => {
     let gameboardArray = [];
+    const getGameboardArray = () => gameboardArray;
     for (let i = 0; i < 9; i++) {
         gameboardArray.push("")
     };
@@ -21,17 +22,50 @@ const game = (() => {
         gameboardDiv.innerHTML = "";
     };
 
-    return {resetBoard, drawBoard,}
+    const updateBoard = () => {
+        gameboardDiv.innerHTML = "";
+        drawBoard();
+    }
+
+    return {resetBoard, drawBoard, updateBoard, getGameboardArray}
 })();
+
+game.drawBoard();
 
 const player = (name, mark) => {
     let score = 0;
     const getScore = () => score;
     const getMark = () => mark;
     const getName = () => name;
-    return {getName, getMark, getScore, updateName}
+    return {getName, getMark, getScore}
 };
 
 const player1 = player(document.getElementById("player1-name").innerHTML, '✖');
 const player2 = player(document.getElementById("player2-name").innerHTML, '〇');
 let playerPlaying;
+
+const gameControler = (() => {
+    
+    let turn = 1;
+    const getTurn = () => turn;
+
+    const playerTurn = () => {
+        (!(turn%2 == 0)) ? playerPlaying = player1 : playerPlaying = player2;     
+    }
+    
+    
+    
+    const fields = [... document.getElementsByClassName("gameboard-field")];
+    fields.forEach((field) => {
+        field.addEventListener('click', () => { 
+            if (field.textContent === "") { 
+            playerTurn();
+            field.textContent = playerPlaying.getMark();
+            turn++;
+            let fieldNumber = field.getAttribute('data-key');
+            game.getGameboardArray()[fieldNumber] = field.textContent;
+            }
+        })
+    })
+
+})();
